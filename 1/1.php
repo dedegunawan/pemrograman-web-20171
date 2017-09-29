3ef004069e1525df1289ac582a38e14b71c025d1
Dede Gunawan <dede@unsil.ac.id>
2017-09-29 09:20:36 +0700

    Pertemuan Pertama

---
+++ 1/1.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+  <title>Contoh Skrip PHP</title>
+</head>
+<body>
+  <?php
+  $nama = "Dede Gunawan";
+  echo "$nama berkata \"Katakan Namamu\" ";
+   ?>
+</body>
+</html>
---
+++ 1/3.php
@@ -0,0 +1,12 @@
+<?php
+$a = 5;
+$b = 7;
+echo "a = $a<br />";
+echo "b = $b<br />";
+if ($a == $b) {
+  echo "$a sama dengan $b<br />";
+} elseif ($a > $b) {
+  echo "$a lebih besar dari $b<br />";
+} else {
+  echo "$b lebih besar dari $a<br />";
+}
