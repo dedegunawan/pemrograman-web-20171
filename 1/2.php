ef5669d2a9aba31b374d2efc920239a0e1821aea
unsil <unsil@iMac-Lab-Jarkom.local>
2017-09-30 08:15:24 +0700

    Tambahan Pertemuan 1

---
+++ 1/2.php
@@ -0,0 +1,9 @@
+<?php
+
+$a = 3;
+$b = 7;
+$a += 5;
+$b = ($c = 11) + 3;
+echo "Nilai variabel \$a = $a<br />";
+echo "Nilai variabel \$b = $b<br />";
+echo "Nilai variabel \$c = $c<br />";
---
+++ 1/4.php
@@ -0,0 +1,18 @@
+<?php
+
+$a = 5;
+switch ($a) {
+    case 0:
+        echo "\$a ($a) sama dengan 0";
+        break;
+    case 1:
+        echo "\$a ($a) sama dengan 1";
+        break;
+    case 2:
+        echo "\$a ($a) sama dengan 2";
+        break;
+
+    default:
+        echo "\$a ($a) tidak sama dengan 0,1,2";
+        break;
+}
